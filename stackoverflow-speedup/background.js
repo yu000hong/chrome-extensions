42dd6eef5cd2afb7dffa0b5a4180bf89f544abde
余红 <yuhong4@staff.weibo.com>
2017-09-15 12:26:35 +0800

    init

---
+++ stackoverflow-speedup/background.js
@@ -0,0 +1,14 @@
+var sourceUrl = 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js';
+var targetUrl = 'https://code.jquery.com/jquery-3.2.1.min.js';
+// console.log("hello ...");
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+    	if(details.url == sourceUrl){
+    		return {redirectUrl:targetUrl};
+    	}else{
+    		return {cancel:false};
+    	}
+    	// console.log(details.url);
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking"]);
